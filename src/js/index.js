da62445025ae749c4d552c94d4f4ac142efafbd4
Rodrigo Costa <rodrigojesusdacosta@gmail.com>
2026-02-02 20:46:06 -0300

    refactor: refactor validation

--- src/js/index.js
+++ src/js/index.js
@@ -3,9 +3,11 @@ const percentageNumbers = document.querySelectorAll('.percentage-number')
 const resetButton = document.querySelector('.result-section__button')
 const billInput = document.querySelector('#bill-input')
 const peopleInput = document.querySelector('#people-number-input')
-const zeroErrorText = document.querySelector('#zero-error')
+const zeroBillErrorText = document.querySelector('#bill-zero-error')
+const zeroPeopleErrorText = document.querySelector('#people-zero-error')
 const billNegativeErrorText = document.querySelector('#bill-negative-error')
 const peopleNegativeErrorText = document.querySelector('#people-negative-error')
+const manyPeopleErrorText = document.querySelector('#not-that-many-people-error')
 
 const totalTipPerPerson = document.querySelector('#total-tip-value')
 const totalValuePerPerson = document.querySelector('#total-per-person')
@@ -14,6 +16,12 @@ let selectedPercentage = 0
 
 billInput.addEventListener('input', function () {
     let bill = Number(billInput.value)
+
+    if (bill === 0) {
+        zeroBillErrorText.classList.remove('hidden')
+    } else {
+        zeroBillErrorText.classList.add('hidden')
+    }
 
     if (bill < 0) {
         billNegativeErrorText.classList.remove('hidden')
@@ -32,15 +40,21 @@ peopleInput.addEventListener('input', function () {
     let people = Number(peopleInput.value)
 
     if (people === 0) {
-        zeroErrorText.classList.remove('hidden')
+        zeroPeopleErrorText.classList.remove('hidden')
     } else {
-        zeroErrorText.classList.add('hidden')
+        zeroPeopleErrorText.classList.add('hidden')
     }
 
     if (people < 0) {
         peopleNegativeErrorText.classList.remove('hidden')
     } else {
         peopleNegativeErrorText.classList.add('hidden')
+    }
+
+    if(people >= 999){
+        manyPeopleErrorText.classList.remove('hidden')
+    } else{
+        manyPeopleErrorText.classList.add('hidden')
     }
 
     if (selectedPercentage != 0) {
@@ -57,7 +71,7 @@ function calculation() {
     let bill = Number(billInput.value)
     let people = Number(peopleInput.value)
 
-    if (bill != 0 && bill > 0 && people != 0 && people > 0) {
+    if (bill != 0 && bill > 0 && people != 0 && people > 0 && people < 999) {
         let totalTip = (bill * (selectedPercentage / 100))
         totalTipPerPerson.textContent = (totalTip / people).toFixed(2)
 
@@ -89,7 +103,7 @@ tipButtons.forEach(function (number) {
 })
 
 
-resetButton.addEventListener('click', function(){
+function reset() {
 
     tipButtons.forEach(function (button) {
         button.classList.remove('active-button')
@@ -100,4 +114,17 @@ resetButton.addEventListener('click', function(){
 
     totalTipPerPerson.textContent = '0.00'
     totalValuePerPerson.textContent = '0.00'
+
+    zeroBillErrorText.classList.add('hidden')
+    billNegativeErrorText.classList.add('hidden')
+    zeroPeopleErrorText.classList.add('hidden')
+    peopleNegativeErrorText.classList.add('hidden')
+    manyPeopleErrorText.classList.add('hidden')
+
+    selectedPercentage = 0
+
+}
+
+resetButton.addEventListener('click', function(){
+    reset()
 })
